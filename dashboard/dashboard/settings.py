5e9d64ae3f9590d4a9a4a788aea02c1580d3735e
Isaac Jordan <sheepses6@gmail.com>
2015-11-11 15:22:25 +0000

    Added dashboard app to settings.

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -120,6 +120,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.admindocs',
+    'dashboard'
 )
 
 # A sample logging configuration. The only tangible logging
