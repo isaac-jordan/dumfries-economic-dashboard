df33b531bbeb8553d6a53a5dc082692be212d1bf
Isaac Jordan <sheepses6@gmail.com>
2016-01-19 16:04:37 +0000

    Major restructuring of project into multiple apps. BE CAREFUL WHEN MERGING THIS WITH NEW CHANGES.

--- dashboard/dashboard/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-class BasicTestCase(TestCase):
-    def test_tester(self):
-        """Test to identify that unit tests are running"""
-        self.assertEqual(True, 5 == 5)
-        self.assertEqual(False, 5 == 6)
