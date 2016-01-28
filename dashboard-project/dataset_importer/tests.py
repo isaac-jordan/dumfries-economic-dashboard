24e6043c520625befa7afe4b9ba9cf0902c5795d
Isaac Jordan <sheepses6@gmail.com>
2016-01-28 10:22:32 +0000

    Added coverage test for dataset_importer models.

--- dashboard-project/dataset_importer/tests.py
+++ dashboard-project/dataset_importer/tests.py
@@ -6,11 +6,22 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from models import Dataset
+import json
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class ImporterTest(TestCase):
+    def setUp(self):
+        Dataset.objects.get_or_create(name="test_dataset",
+                                      dataJSON="""[{"x":"test"}]""")
+        Dataset.objects.get_or_create(dataJSON="""[{"y":"test"}]""")
+        
+    def test_dataset_json(self):
+        dataset = Dataset.objects.get(name="test_dataset")
+        self.assertEqual(json.loads(dataset.dataJSON), [{"x":"test"}], "Dataset JSON did not load correctly.")
+        
+    def test_dataset_str(self):
+        dataset = Dataset.objects.get(name="test_dataset")
+        self.assertTrue(str(dataset) == dataset.name, "Dataset did not return correct name from __str__")
+        dataset = Dataset.objects.get(pk=2)
+        self.assertTrue(str(dataset) == "Imported Dataset " + str(dataset.pk), "Dataset without name did not return correct name from __str__")
