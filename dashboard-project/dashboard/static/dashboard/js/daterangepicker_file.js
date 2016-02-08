fae540d1d4a32754a8a5194a27e16bc3dcd66ede
Branko Kourtellos <2060408k@student.gla.ac.uk>
2016-02-08 14:36:00 +0000

    Graphs start from 1990 until today.
    Reference: #52
    
    -Daterangepicker_file.js :
        >Changed startDate to 01/01/1990
        >changed endDate to current date

--- dashboard-project/dashboard/static/dashboard/js/daterangepicker_file.js
+++ dashboard-project/dashboard/static/dashboard/js/daterangepicker_file.js
@@ -6,8 +6,8 @@ var applyDatePickerStats=function(){
 	    "autoApply": true,
 	    "maxDate":"+10Y",//Works, don't know why, but it does !
 	    "autoUpdateInput": true,
-	    "startDate": "01/13/2010",
-	    "endDate": "01/19/2016",
+	    "startDate": "01/01/1990",
+	    "endDate": new Date(),
 	    "opens": "left"
 	}, function(start, end, label) {
 	  console.log("New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')");
