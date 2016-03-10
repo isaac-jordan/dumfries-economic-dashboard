4b8d9e88c964f8da7248330ff3503d541fa62d16
Branko Kourtellos <2060408k@student.gla.ac.uk>
2016-03-10 11:50:01 +0000

    refs #74
    Clean up of graph.js
    
    graphs.js:
        >Cleaned the "magic constants"
        >Deleted unused code
        >Commented some code
        >Fixed minor bugs

--- dashboard-project/dashboard/static/dashboard/js/graphs.js
+++ dashboard-project/dashboard/static/dashboard/js/graphs.js
@@ -1,5 +1,5 @@
 /*jshint*/
-/*global d3, console, GLOBAL */
+/*global d3, console */
 
 function drawGraph(elemID, data, type,datasetLabels,xLabel,yLabel) {
     if ($("#" + elemID).parent().width() < 0) return;
@@ -15,10 +15,11 @@ function drawGraph(elemID, data, type,datasetLabels,xLabel,yLabel) {
 
 function bargraph(elemID, data,xLabel,yLabel) {
     var HEIGHT=0;
-    if ($("#" + elemID).parent().parent().parent().height()-90>1000){
-        HEIGHT = 300;
+    var tool_bar_height=100;
+    if ($("#" + elemID).parent().parent().parent().height()-tool_bar_height>1000){
+        HEIGHT = 500;
     }else{
-        HEIGHT = $("#" + elemID).parent().parent().parent().height()-90;
+        HEIGHT = $("#" + elemID).parent().parent().parent().height()-tool_bar_height;
     }
     data = data[0];
     $("#" + elemID).empty();
@@ -63,23 +64,40 @@ function bargraph(elemID, data,xLabel,yLabel) {
         .text(function (d) {
             return (d.x+" "+d.y);
         });
-    //console.log(yLabel);
 }
-
+function add_Trend_Element(elemID){
+    var vis = d3.select("#" + elemID);
+    vis.append("text")
+        .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
+        .attr("transform", "translate("+ ($("#" + elemID).width()-100) +","+(($("#" + elemID).height()-250))+")")  // centre below axis
+        .text("Highest Value was : 500")
+        .append('svg:tspan')
+        .attr('x', -40)
+        .attr('dy', 15)
+        .text("Now is : 300")
+        .append('svg:tspan')
+        .attr('x', -40)
+        .attr('dy', 15)
+        .text("Lowest was : 100");
+}
 function cleanup_data(data, type, clean_data){
-    var end_date = GLOBAL.endDateRange;
-    var start_date = GLOBAL.startDateRange;
+    var dates=$('input[name="daterange"]').val();//get daterangepicker's value
+    var end_date=new Date();
+    var start_date=new Date();
     var i,d, c_date, input;
 
-    if (!end_date || !start_date) {
-        start_date = new Date();
+    if (dates) {
+        var e_date = dates.slice(-4);
+        end_date.setYear(e_date);
+
+        var s_date =dates.substring(6,10);
+        start_date.setYear(s_date);
+    } else {
         start_date.setYear(1900);
-        
-        end_date = new Date();
         end_date.setYear(2100);
     }
-
-    if (type=="normal"){
+    //Check date type
+    if (type=="normal"){ //Yearly
         for (i=0; i<data.length; i++) {
             for (d=0;d<data[i].length; d++) {
                 c_date=new Date();
@@ -92,7 +110,7 @@ function cleanup_data(data, type, clean_data){
                 }
             }
         }
-    } else {
+    } else {//Date object
         for (i=0; i<data.length; i++) {
             for (d=0;d<data[i].length; d++) {
                 c_date=new Date(data[i][d].x);
@@ -105,25 +123,28 @@ function cleanup_data(data, type, clean_data){
             }
         }
     }
+
     return clean_data;
 }
-
 function add_legend(elemID,vis,data,colours,datasetLabels,MARGINS){
-    var y=0;
+    y=0;
+    data_width=40;
+    rect_height=12;
+    //for every data add coressponidng
+    for (i=0;data[i]!=null ;i++) {
 
-    for (var i=0; data[i] !== null && i < data.length; i++) {;
         vis.append("rect")
             .attr("width",30)
-            .attr("height",12)
+            .attr("height",rect_height)
             .attr("x",MARGINS.left)
             .attr("y",y)
             .attr("dy","-3.5em")
             .attr('fill',colours[i]);
         vis.append("text")
-            .attr("x",MARGINS.left+40)
-            .attr("y",y+12)
+            .attr("x",MARGINS.left+data_width)
+            .attr("y",y+rect_height)
             .text(datasetLabels[i]);
-        y+=17;
+        y+=17
     }
 }
 function linegraph(elemID, data,datasetLabels,xLabel,yLabel) {
@@ -131,16 +152,20 @@ function linegraph(elemID, data,datasetLabels,xLabel,yLabel) {
     var c_data=data;//Copy of our data given in
     var type="normal";
     var clean_data=[];
+
     $("#" + elemID).empty();// Clean previous graph in widget
-    for (var i=0;i<data.length;i++) {
+
+    for (var i=0;i<data.length;i++) {//Initialize the number of lines
         clean_data[i] = [];
     }
+
     if (!data[0][0]) return;
+
     if (typeof(data[0][0].x)=="string") type="date_format"; //Temporary solution for the different date formats
 
     clean_data=cleanup_data(c_data,type,clean_data);// Sends a copy of our data to be filtered and converts dates to JS Date format
-    //console.log("Finished cleanup.");
-    if (clean_data[0].length === 0 ) { //It does not add the text !!
+
+    if (clean_data[0].length === 0 ) {
         vis = d3.select("#" + elemID).attr('width', $("#" + elemID).parent().width()).attr('height', 270);
         vis.append("text")
             .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
@@ -155,7 +180,6 @@ function linegraph(elemID, data,datasetLabels,xLabel,yLabel) {
     var x=0;
 
     //Date comparison to find min and max values
-    //console.log("Starting date comparison.");
     for (x=0;x<clean_data.length;x++){
         for (i=0; i<clean_data[x].length; i++) {
 
@@ -170,39 +194,34 @@ function linegraph(elemID, data,datasetLabels,xLabel,yLabel) {
             if (yMaxCurr > yMax) yMax = yMaxCurr;
         }
     }
-    //console.log("Finished date comparison.");
-    //For now just get the year value
+    //Get the year value
     xMin=xMin.getFullYear();
     xMax=xMax.getFullYear();
 
-    //console.log("Starting d3 code.");
+
     var HEIGHT=0;
-    if ($("#" + elemID).parent().parent().parent().height()-90>1000){
+    var tool_bar_height=100;
+    if ($("#" + elemID).parent().parent().parent().height()-tool_bar_height>1000){
         HEIGHT = 300;
     }else{
-        HEIGHT = $("#" + elemID).parent().parent().parent().height()-90;
+        HEIGHT = $("#" + elemID).parent().parent().parent().height()-tool_bar_height;
     }
-    var WIDTH = $("#" + elemID).parent().width(),
+    var WIDTH = $("#" + elemID).parent().parent().width(),
         colours = ['#00264d', ' #0064cc' , '#0066cc',' #3399ff',' #fff'],
 
         MARGINS = {
             top: 30,
-            right: 5,
+            right: 25,
             bottom: data.length*10+10,
             left: 60
-        },
-        xScale = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([xMin, xMax]),
-        yScale = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([yMin, yMax]),
-        xAxis = d3.svg.axis()
-            .scale(xScale)
-            .tickFormat(d3.format(xAxis)),
-        yAxis = d3.svg.axis()
-            .scale(yScale)
-            .orient("left");
+        }
+
+    //Create svg element
     vis = d3.select("#" + elemID).attr("width", WIDTH).attr("height", HEIGHT);
 
     xScale = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([xMin, xMax]);
     yScale = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([yMin, yMax]);
+
     if (clean_data[0].length==1) {// In case of 1 element margin domain changes
         xScale = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([xMin-1, xMax+1]);
         yScale = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([yMax/2, yMax*2]);
@@ -210,44 +229,45 @@ function linegraph(elemID, data,datasetLabels,xLabel,yLabel) {
     }else if(type!="normal"){
         xScale = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([xMin, xMax+1]);
     }
-    if (type!="normal") {}
-    var datelength=xMax-xMin;
+
+    //Specify x axis
     xAxis = d3.svg.axis()
         .scale(xScale)
         .tickFormat(d3.format("date"))
         .orient("bottom")
         .ticks((WIDTH-90)/30);
+
+    //Specify y axis
     yAxis = d3.svg.axis()
         .scale(yScale)
         .orient("left");
 
+    //Add y axis
     vis.append("svg:g")
         .attr("class", "y axis")
         .attr("transform", "translate(" + MARGINS.left + ",0)")
         .call(yAxis);
 
+    //Add x axis
     vis.append("svg:g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + (HEIGHT - MARGINS.top) + ")")
         .call(xAxis);
 
-
-
-
+    //Line generator
     var lineGen = d3.svg.line()
         .x(function(d) {
             return xScale(d.x.getFullYear()+(d.x.getMonth())/12);
         })
         .y(function(d) {
             return yScale(d.y);
-        });
-    
+        })
     var div = d3.select("body")
         .append("div")
         .attr("class", "tooltip")
         .style("opacity", 0);
-    
     var bisectDate = d3.bisector(function(d) { return d.y; }).left;
+
     //In case of 1 element we append a circle with just a text
     if (clean_data[0].length==1){
         vis.append('circle')
@@ -259,19 +279,9 @@ function linegraph(elemID, data,datasetLabels,xLabel,yLabel) {
             .text(clean_data[0][0].y+" for "+clean_data[0][0].x.getFullYear());
         return;
     }
-    
-    var focus = vis.append("g")
-        .attr("class", "focus")
-        .style("display", "none");
 
-    focus.append("circle")
-        .attr("r", 4.5);
 
-    focus.append("text")
-        .attr("x", 9)
-        .attr("dy", ".35em");
-    
-    //console.log("Starting last clean_data loop");
+    //generate the line graph by calling lineGen
     for (i=0; i<clean_data.length; i++) {
         vis.append('svg:path')
             .attr('d', lineGen(clean_data[i]))
@@ -279,75 +289,47 @@ function linegraph(elemID, data,datasetLabels,xLabel,yLabel) {
             .attr('stroke-width', 2)
             .attr('fill', 'none')
             .on("mousemove", function(d) {
-                        div.transition()
-                            .duration(10)
-                            .style("opacity", 1)
-                            .delay(1000)
-                            .style("opacity",0);
-                    })
-            .on("mouseover", mousemove);
-    }
-    //console.log("Finished last clean_data loop");
+                div.transition()
+                    .duration(10)
+                    .style("opacity", 1)
+                    .delay(1000)
+                    .style("opacity",0);
+            })
+            .on("mouseover", mousemove)
 
-    function kiklos() {
-        for (var a = 0; a < clean_data.length; a++) {
-            for (var i = 0; i < clean_data[a].length; i++) {
-                vis.append('circle')
-                    .attr("cx", xScale(clean_data[a][i].x.getFullYear()+(clean_data[a][i].x.getMonth())/12))
-                    .attr("cy", yScale(clean_data[a][i].y))
-                    .attr("r", 4.5);
-            }
-        }
     }
-    
+
+
     function mousemove() {
-        var a = 0;
+        var a=0;
         for (a = 0; a < clean_data.length; a++) {
-
             var x0 = xScale.invert(d3.mouse(this)[0]).toFixed(0);
             var y = yScale.invert(d3.mouse(this)[1]).toFixed(0),
                 i = bisectDate(clean_data, x0, a),
                 d0 = clean_data[i - 1],
                 d1 = clean_data[i + a],
                 d = x0 - d0 > d1 - x0 ? d1 : d0;
-            //console.log(yLabel);
             var x2 = Math.round(xScale(x0));
             var y2 = Math.ceil(y / 10) * 10;
             div.transition()
                 .duration(10)
-                .style("opacity",0.9);
+                .style("opacity",0.9)
             div.html(yLabel+"<b/>" +": " + y+  "<br/>"  + xLabel +": " +x0)
 
                 .style("left", (d3.event.pageX) + "px")
-                .style("top", (d3.event.pageY - 28) + "px");
-
-
-            /*vis.append('circle')
-             .attr('fill', "transparent")
-             .attr('stroke', colours[i])
-             .attr('stroke-width', 2)
-             .attr("cx", xScale(x0) )
-             .attr("cy",yScale(y2) )
-
-             .attr("r", 3.5);      }
-             */
+                .style("top", (d3.event.pageY - 28) + "px")
         }
     }
-    
-    //console.log("Adding legend");
-    if(datasetLabels){
+
+    //Check that labels for the dataset exist and then add them
+    if(typeof datasetLabels !== 'undefined' || datasetLabels !=null){
         add_legend(elemID,vis,clean_data,colours,datasetLabels,MARGINS);
     }
-    //console.log("Finished adding legend");
-    
-    //console.log("Adding axis labels");
     add_axis_labels(vis,xLabel,yLabel,WIDTH,HEIGHT,MARGINS);
-    //console.log("Finished adding axis labels");
-    
-    //console.log("Finished d3 code.");
 
 }
 
+//Add the axis labels to the linegraphs
 function add_axis_labels(vis,xLabel,yLabel,WIDTH,HEIGHT,MARGINS){
 
     vis.append("text")
